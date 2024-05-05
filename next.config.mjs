c4a2758f497726a36a332e48663d3e77f9080706
RaminKh <kh.ramin69@gmail.com>
2024-05-05 20:12:19 +0330

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    rewrites() {
+        return [
+            {
+                source: process.env.NEXT_PUBLIC_BASE_URL_RELATIVE + '/:path*',
+                destination: process.env.NEXT_PUBLIC_BASE_URL_DIRECT + '/:path*',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
